e12c1f87dd793f76e089e945064d6bcb0af39e08
wei <a20161104571@mac18.local>
2018-10-10 10:05:20 +0800

    Initial Commit

---
+++ calculator_4571/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator_4571
+//
+//  Created by 20161104571 on 2018/10/10.
+//  Copyright © 2018年 20161104571. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
